29f74141fabc3fed0319c6e4a70b0ae5a58a23e8
Samir Gamal <69224131+Samir-Gamal@users.noreply.github.com>
2021-04-13 00:33:56 +0300

    Insert_Update_Delete_Table_Sections

--- resources/lang/ar/Dashboard/sections_trans.php
+++ resources/lang/ar/Dashboard/sections_trans.php
@@ -2,14 +2,14 @@
 
 return [
 
-   'add_sections'=>'Add Sections',
-   'name_sections'=>'Name Sections',
-    'edit_sections'=>'Edit Sections',
-    'delete_sections'=>'Delete Sections',
-   'created_at'=>'created_at',
-   'Processes'=>'Processes',
-   'submit'=>'Submit',
-   'Close'=>'Close',
-   'Warning'=>'Are Sure Of The Deleting Process ?',
+   'add_sections'=>'اضافة قسم',
+   'name_sections'=>'اسم القسم',
+    'edit_sections'=>'تعديل قسم',
+    'delete_sections'=>'حذف قسم',
+   'created_at'=>'تاريخ الاضافة',
+   'Processes'=>'العمليات',
+   'submit'=>'تاكيد',
+   'Close'=>'اغلاق',
+   'Warning'=>'هل انت متاكد من عملية الحذف ؟',
 
 ];
---
+++ resources/lang/en/Dashboard/sections_trans.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+   'add_sections'=>'Add Sections',
+   'name_sections'=>'Name Sections',
+    'edit_sections'=>'Edit Sections',
+    'delete_sections'=>'Delete Sections',
+   'created_at'=>'Created_at',
+   'Processes'=>'Processes',
+   'submit'=>'Submit',
+   'Close'=>'Close',
+   'Warning'=>'Are Sure Of The Deleting Process ?',
+
+];
