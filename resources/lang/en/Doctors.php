ffbe2d0f8e3662609556521aa7dee46d81d53c2b
Samir Gamal <69224131+Samir-Gamal@users.noreply.github.com>
2021-04-29 23:35:37 +0300

    Delete_Table_Doctor_&_images

--- resources/lang/en/Doctors.php
+++ resources/lang/en/Doctors.php
@@ -1,19 +1,5 @@
 <?php
 
 return array (
-  'Enabled' => 'Enabled',
-  'Not_enabled' => 'Not_enabled',
-  'Processes' => 'Processes',
-  'Status' => 'Status',
-  'add_doctor' => 'Add Doctor',
-  'appointments' => 'appointments',
-  'created_at' => 'created_at',
-  'doctor_photo' => 'Doctor Photo',
-  'email' => 'Email',
-  'name' => 'Doctor Name',
-  'password' => 'password',
-  'phone' => 'phone',
-  'price' => 'price',
-  'section' => 'Section',
-  'submit' => 'submit',
+  'img' => 'img',
 );
