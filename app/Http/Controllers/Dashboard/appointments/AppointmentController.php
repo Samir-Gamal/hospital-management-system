e86a81d22e78438f81735f2acdb30226d372d688
Samir Gamal <samir.gamal77@yahoo.com>
2023-01-14 17:38:46 +0300

    send_appointment_details_on_mobile

--- app/Http/Controllers/Dashboard/appointments/AppointmentController.php
+++ app/Http/Controllers/Dashboard/appointments/AppointmentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Dashboard\appointments;
 
 use App\Http\Controllers\Controller;
+use App\Mail\AppointmentConfirmation;
 use App\Models\Appointment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Twilio\Rest\Client;
 
 class AppointmentController extends Controller
 {
@@ -25,6 +28,22 @@ class AppointmentController extends Controller
         $appointment->update([
             'type'=>'مؤكد',
             'appointment'=>$request->appointment
+        ]);
+
+        // send email
+        Mail::to($appointment->email)->send(new AppointmentConfirmation($appointment->name,$appointment->appointment));
+
+        // send message mob
+        $receiverNumber = $appointment->phone;
+        $message = "عزيزي المريض" . " " . $appointment->name . " " . "تم حجز موعدك بتاريخ " . $appointment->appointment;
+
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_TOKEN");
+        $twilio_number = getenv("TWILIO_FROM");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create($receiverNumber,[
+            'from' => $twilio_number,
+            'body' => $message
         ]);
         session()->flash('add');
         return back();
